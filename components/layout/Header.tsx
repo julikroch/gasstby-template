6033314d9e629c6df01320c7111deaf00e92771a
Julian Krochmal <juliankrochmal@gmail.com>
2022-02-02 16:06:49 -0300

    fixing ts config file

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,22 +2,75 @@ import Link from 'next/link';
 import React from 'react';
 import Search from '../ui/Search';
 import Navigation from './Navigation';
+import styled from '@emotion/styled';
+import { jsx, css } from '@emotion/react';
+import Button from '../ui/Button'
+
+const HeaderContainer = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: "Robot Slab", serif;
+    margin-right: 2rem;
+`
 
 const Header = () => {
+
+    const user = false
+
     return (
-        <header>
-            <div>
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+            `}
+        >
+            <HeaderContainer>
                 <div>
+                    <Link href={'/'}>
+                        <Logo>P</Logo>
+                    </Link>
                     <Search />
                     <Navigation />
                 </div>
-                <div>
-                    <p>Hi, Julian</p>
-                    <button type="button">Log out</button>
-                    <Link href={"/"}>Login</Link>
-                    <Link href={"/"}>Create Account</Link>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    {user ? (
+                        <>
+                            <p
+                                css={css`
+                                    margin-right: 2rem;
+                                `}
+                            >Hi, Julian</p>
+                            <Button bgColor={true} textColor={true}>Log out</Button>
+                        </>
+                    ) : (
+                        <>
+                            <Link href={"/"}>
+                                <Button bgColor={true} textColor={true}>Login</Button>
+                            </Link>
+                            <Link href={"/"}>
+                                <Button>Create Account</Button>
+                            </Link>
+                        </>)
+                    }
                 </div>
-            </div>
+            </HeaderContainer>
         </header>
     );
 };
---
+++ components/ui/Button.tsx
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+interface Button {
+    bgColor?: boolean;
+    textColor?: boolean;
+}
+
+const Button = styled.a`
+    font-weight: 700;
+    text-transform: uppercase;
+    border: 1px solid #d1d1d1;
+    padding: .8rem 2rem;
+    margin-right: 1rem;
+    background-color: ${(props: Button) => props.bgColor ? '#DA552F' : '#ffffff'};
+    color: ${(props: Button) => props.textColor ? 'white' : 'black'};
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export default Button
